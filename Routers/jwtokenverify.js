681c3f278554e2c162f6b37b9bff82e6e6c4c077
mayur bhoi <mayurbhoi200@gmail.com>
2022-12-25 11:58:03 +0530

    set sms

--- Routers/jwtokenverify.js
+++ Routers/jwtokenverify.js
@@ -8,7 +8,10 @@ async function jwtVerify(req, res, next) {
 
     // console.log(token);
 
-    if (!token) return;
+    if (!token)
+    res.status(401).json({ message: "Authentication failed" });
+     return
+     ;
 
     const user = jwt.verify(token, process.env.SECREATE_KEY);
 
